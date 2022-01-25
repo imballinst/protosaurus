39a5e10b56359e9d2430c2f375d4e98151f5792e
Dhi Aurrahman <dio@tetrate.io>
2022-01-25 18:22:55 +0700

    Add gen target (#1)
    This adds necessary bootstrapping for generating `doc.json` files. The generated files will be stored in `website/generated`.
    
    Upon successful `make gen`,
    
    ```console
    $ tree website/generated/
    website/generated/
    └── booking
        └── v1
            └── doc.json
    ```
    
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

---
+++ README.md
@@ -0,0 +1,23 @@
+# protosaurus
+
+To generate docusaurus documentation website from your `*.proto`.
+
+## Quick start
+
+You need to have [Go 1.17.x](https://go.dev/doc/install) installed on your system, also: `bash`, `make`, `awk`, `tar`, `unzip`, and `curl`.
+
+To generate:
+
+```console
+make gen
+```
+
+To check on more targets:
+
+```console
+make help
+```
+
+## License
+
+See: [LICENSE](./LICENSE).
---
+++ Tools.mk
@@ -0,0 +1,9 @@
+# Copyright 2022 Protosaurus Authors
+# Licensed under the Apache License, Version 2.0 (the "License")
+
+buf@v            := github.com/bufbuild/buf/cmd/buf@v1.0.0-rc6
+clang-format@v   := github.com/angular/clang-format@v1.6.0
+node@v           := nodejs.org/node@v16.13.2
+protoc@v         := github.com/protocolbuffers/protobuf@v3.9.2
+protoc-gen-doc@v := github.com/3128px/protoc-gen-doc/v2/cmd/protoc-gen-doc@v2.0.1
+yarn@v           := github.com/yarnpkg/yarn@v1.22.17
