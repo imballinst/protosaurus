5f5e448def1b1908d2e8ad4371ebc33f5345abe3
Try Ajitiono <ballinst@gmail.com>
2022-01-26 17:05:05 +0700

    still have order issue and all that, but lets see tomorrow
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- mdx/lib/doc-to-mdx.test.ts
+++ mdx/lib/doc-to-mdx.test.ts
@@ -1,5 +1,6 @@
 import { convertPackageToMdx } from "./doc-to-mdx";
 import path from "path";
+import assert from "assert";
 
 const ROOT_PATH = path.join(__dirname, "../../");
 const BOOKING_DOC_JSON_PATH = path.join(
@@ -9,5 +10,45 @@ const BOOKING_DOC_JSON_PATH = path.join(
 
 test("convertPackageToMdx", async () => {
   const result = await convertPackageToMdx(BOOKING_DOC_JSON_PATH);
+  const expected = `
+// Represents the booking status ID.
+message BookingStatusID {
+  // Unique booking status ID.
+  int32 id = 1;
+}
+
+// Represents the status of a vehicle booking.
+message BookingStatus {
+  // Unique booking status ID.
+  int32 id = 1;
+  // Booking status description. E.g. "Active".
+  string description = 2 [(google.api.field_behavior) = REQUIRED, (validate.rules).string.min_len = 1];
+}
+
+// Represents the booking of a vehicle.
+//
+// Vehicles are some cool shit. But drive carefully.
+message Booking {
+  // ID of booked vehicle.
+  int32 vehicle_id = 1;
+  // Customer that booked the vehicle.
+  int32 customer_id = 2;
+  // Status of the booking.
+  BookingStatus status = 3;
+
+  // Has booking confirmation been sent.
+  bool confirmation_sent = 4;
+
+  // Has payment been received.
+  bool payment_received = 5;
+
+  // Color preference of the customer.
+  string color_preference = 6 [deprecated = true];
+}
+
+// An empty message for testing
+message EmptyBookingMessage {}`.trim();
   console.log(result);
+  console.log(expected);
+  assert.equal(result, expected);
 });
--- mdx/lib/doc-to-mdx.ts
+++ mdx/lib/doc-to-mdx.ts
@@ -1,8 +1,156 @@
 import fs from "fs-extra";
 
+interface Protofile {
+  name: string;
+  title: string;
+  description: string;
+  package: string;
+  hasEnums: false;
+  hasExtensions: boolean;
+  hasMessages: boolean;
+  hasServices: boolean;
+  // TODO(imballinst): create proper typing.
+  enums: any;
+  extensions: any;
+  messages: ProtoMessage[];
+  services: ProtoService[];
+}
+
+interface ProtoMessage {
+  name: string;
+  longName: string;
+  fullName: string;
+  description: string;
+  hasExtensions: boolean;
+  hasFields: boolean;
+  hasOneofs: boolean;
+  extensions: any[];
+  fields: Field[];
+}
+
+interface Field {
+  name: string;
+  description: string;
+  label: string;
+  type: string;
+  longType: string;
+  fullType: string;
+  ismap: boolean;
+  isoneof: boolean;
+  oneofdecl: string;
+  defaultValue: string;
+  options?: {
+    [index: string]: any;
+  };
+}
+
+interface ProtoService {
+  name: string;
+  longName: string;
+  fullName: string;
+  description: string;
+  methods: {
+    name: string;
+    description: string;
+    requestType: string;
+    requestLongType: string;
+    requestFullType: string;
+    requestStreaming: boolean;
+    responseType: string;
+    responseLongType: string;
+    responseFullType: string;
+    responseStreaming: boolean;
+    options: any;
+  };
+}
+
+const x = [
+  {
+    name: "BookingService",
+    longName: "BookingService",
+    fullName: "booking.v1.BookingService",
+    description: "Service for handling vehicle bookings.",
+    methods: [
+      {
+        name: "BookVehicle",
+        description:
+          "Used to book a vehicle. Pass in a Booking and a BookingStatus will be\nreturned.",
+        requestType: "Booking",
+        requestLongType: "Booking",
+        requestFullType: "booking.v1.Booking",
+        requestStreaming: false,
+        responseType: "BookingStatus",
+        responseLongType: "BookingStatus",
+        responseFullType: "booking.v1.BookingStatus",
+        responseStreaming: false,
+        options: {
+          "google.api.http": {
+            rules: [
+              {
+                method: "POST",
+                pattern: "/api/bookings/vehicle/{vehicle_id}",
+                body: "*",
+              },
+            ],
+          },
+        },
+      },
+      {
+        name: "BookingUpdates",
+        description: "Used to subscribe to updates of the BookingStatus.",
+        requestType: "BookingStatusID",
+        requestLongType: "BookingStatusID",
+        requestFullType: "booking.v1.BookingStatusID",
+        requestStreaming: false,
+        responseType: "BookingStatus",
+        responseLongType: "BookingStatus",
+        responseFullType: "booking.v1.BookingStatus",
+        responseStreaming: true,
+      },
+    ],
+  },
+];
+
 export async function convertPackageToMdx(packagePath: string) {
   const content = await fs.readFile(packagePath, "utf-8");
-  const json = JSON.parse(content);
+  const json: {
+    files: Protofile[];
+  } = JSON.parse(content);
+  let text = "";
 
-  return json;
+  for (const file of json.files) {
+    if (!file.messages) {
+      continue;
+    }
+
+    for (const message of file.messages) {
+      text += getMessageString(message);
+      text += "\n\n";
+    }
+  }
+
+  return text;
+}
+
+function getMessageString(message: ProtoMessage) {
+  return `
+${getCommentString(message.description)}
+message BookingStatus {
+${message.fields.map((field, idx) => getField(field, idx + 1)).join("\n")}
+}
+  `.trim();
+}
+
+function getField(field: Field, num: number) {
+  return `  ${getCommentString(field.description)}
+  ${field.type} ${field.name} = ${num};`;
+}
+
+function getCommentString(comment: string) {
+  const lines = comment.split("\n");
+  for (let i = 0; i < lines.length; i++) {
+    lines[i] = `// ${lines[i]}`;
+  }
+
+  return lines.join("\n");
 }
