805b1a0a214de66fb55002e1259e2f1d5ed8bd19
Dhi Aurrahman <dio@tetrate.io>
2022-01-27 15:17:02 +0700

    Add license and publish (#4)
    This upgrades docusaurus to 2.0.0-beta.15, add `make license` (to https://github.com/google/addlicense to files without a license), also publish the site to gh-pages.
    
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- Tools.mk
+++ Tools.mk
@@ -1,6 +1,7 @@
 # Copyright 2022 Protosaurus Authors
 # Licensed under the Apache License, Version 2.0 (the "License")
 
+addlicense@v     := github.com/google/addlicense@2b44b367595c
 buf@v            := github.com/bufbuild/buf/cmd/buf@v1.0.0-rc6
 clang-format@v   := github.com/angular/clang-format@v1.6.0
 node@v           := nodejs.org/node@v16.13.2
