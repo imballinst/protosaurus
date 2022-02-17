af30540a4dd9dabd5bf7686480880dd210d0f5cc
Try Ajitiono <ballinst@gmail.com>
2022-02-17 13:37:30 +0700

    update protosaurus-theme
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- packages/protosaurus-theme/src/theme/ProtosaurusImage.tsx
+++ packages/protosaurus-theme/src/theme/ProtosaurusImage.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "react-modal";
 
 import styles from "./ProtosaurusImage.module.css";
