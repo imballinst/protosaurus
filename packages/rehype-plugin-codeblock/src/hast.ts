ffbc6995b637792299d9da264d63a8c6ea114b71
Try Ajitiono <ballinst@gmail.com>
2022-03-01 16:01:57 +0700

    use outlined
    Signed-off-by: Try Ajitiono <ballinst@gmail.com>

--- packages/rehype-plugin-codeblock/src/hast.ts
+++ packages/rehype-plugin-codeblock/src/hast.ts
@@ -73,12 +73,17 @@ export function getHastElementType(
 }
 
 export function getInfoSvgIcon(): Element {
-  // Source element: https://chakra-ui.com/docs/media-and-icons/icon.
-  // <svg viewBox="0 0 24 24" focusable="false" class="chakra-icon css-onkibi">
-  //   <path
-  //     fill="currentColor"
-  //     d="M12,0A12,12,0,1,0,24,12,12.013,12.013,0,0,0,12,0Zm.25,5a1.5,1.5,0,1,1-1.5,1.5A1.5,1.5,0,0,1,12.25,5ZM14.5,18.5h-4a1,1,0,0,1,0-2h.75a.25.25,0,0,0,.25-.25v-4.5a.25.25,0,0,0-.25-.25H10.5a1,1,0,0,1,0-2h1a2,2,0,0,1,2,2v4.75a.25.25,0,0,0,.25.25h.75a1,1,0,1,1,0,2Z"
-  //   ></path>
+  // Source element: https://react-icons.github.io/react-icons/icons?name=fa.
+  // <svg
+  //   stroke="currentColor"
+  //   fill="currentColor"
+  //   stroke-width="0"
+  //   viewBox="0 0 192 512"
+  //   height="1em"
+  //   width="1em"
+  //   xmlns="http://www.w3.org/2000/svg"
+  // >
+  //   <path d="M20 424.229h20V279.771H20c-11.046 0-20-8.954-20-20V212c0-11.046 8.954-20 20-20h112c11.046 0 20 8.954 20 20v212.229h20c11.046 0 20 8.954 20 20V492c0 11.046-8.954 20-20 20H20c-11.046 0-20-8.954-20-20v-47.771c0-11.046 8.954-20 20-20zM96 0C56.235 0 24 32.235 24 72s32.235 72 72 72 72-32.235 72-72S135.764 0 96 0z"></path>
   // </svg>
   return {
     type: 'element',
@@ -86,7 +91,7 @@ export function getInfoSvgIcon(): Element {
     properties: {
       stroke: 'currentColor',
       fill: 'currentColor',
-      viewBox: '0 0 24 24',
+      viewBox: '0 0 192 512',
       xmlns: 'http://www.w3.org/2000/svg'
     },
     children: [
@@ -94,7 +99,7 @@ export function getInfoSvgIcon(): Element {
         type: 'element',
         tagName: 'path',
         properties: {
-          d: 'M12,0A12,12,0,1,0,24,12,12.013,12.013,0,0,0,12,0Zm.25,5a1.5,1.5,0,1,1-1.5,1.5A1.5,1.5,0,0,1,12.25,5ZM14.5,18.5h-4a1,1,0,0,1,0-2h.75a.25.25,0,0,0,.25-.25v-4.5a.25.25,0,0,0-.25-.25H10.5a1,1,0,0,1,0-2h1a2,2,0,0,1,2,2v4.75a.25.25,0,0,0,.25.25h.75a1,1,0,1,1,0,2Z'
+          d: 'M20 424.229h20V279.771H20c-11.046 0-20-8.954-20-20V212c0-11.046 8.954-20 20-20h112c11.046 0 20 8.954 20 20v212.229h20c11.046 0 20 8.954 20 20V492c0 11.046-8.954 20-20 20H20c-11.046 0-20-8.954-20-20v-47.771c0-11.046 8.954-20 20-20zM96 0C56.235 0 24 32.235 24 72s32.235 72 72 72 72-32.235 72-72S135.764 0 96 0z'
         },
         children: []
       }
